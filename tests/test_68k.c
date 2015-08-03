281222fc7e2304707458de7bfacb33f0433615b1
Daniel Collin <daniel@collin.com>
2015-08-03 18:48:02 +0200

    Cherry pick 2/3 for 68k support
    Skipped CMake config due to conflicts (will be redone later)

---
+++ tests/test_68k.c
@@ -0,0 +1,12 @@
+/* Capstone Disassembler Engine */
+/* By Nguyen Anh Quynh <aquynh@gmail.com>, 2013-2014 */
+
+#include <stdio.h>
+#include "../myinttypes.h"
+
+#include <capstone.h>
+
+int main()
+{
+	return 0;
+}
