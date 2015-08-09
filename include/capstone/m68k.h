a7a9df26eb9923e26a5a59b8cf6dedbcc57be939
Daniel Collin <daniel@collin.com>
2015-08-09 19:34:24 +0200

    Added some more data for instructions

--- include/capstone/m68k.h
+++ include/capstone/m68k.h
@@ -15,6 +15,32 @@ extern "C" {
 #pragma warning(disable:4201)
 #endif
 
+//> Operand type for instruction's operands
+
+typedef enum m68k_op_type {
+	M68K_OP_INVALID = 0, // = CS_OP_INVALID (Uninitialized).
+	M68K_OP_REG, // = CS_OP_REG (Register operand).
+	M68K_OP_IMM, // = CS_OP_IMM (Immediate operand).
+	M68K_OP_MEM, // = CS_OP_MEM (Memory operand).
+} m68k_op_type;
+
+// Instruction operand
+typedef struct cs_m68k_op {
+	m68k_op_type type;	// operand type
+	union {
+		unsigned int reg;	// register value for REG operand
+		int64_t imm;		// immediate value for IMM operand
+	};
+} cs_m68k_op;
+
+typedef struct cs_m68k
+{
+	// Number of operands of this instruction, 
+	// or 0 when instruction has no operand.
+	uint8_t op_count;
+	cs_m68k_op operands[8]; // operands for this instruction.
+} cs_m68k;
+
 //> M68K registers
 typedef enum m68k_reg {
 	M68K_REG_INVALID = 0,
