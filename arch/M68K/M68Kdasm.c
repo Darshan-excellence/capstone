Darshan-excellence/capstone

commit 0a27c1e7e688413240f35aa10111f5f1a71ba96a

[M68K] add jsr/jmp decoding support
Using build_bxx() helper function to implement jsr/jmp mnemonics.

Signed-off-by: Nicolas PLANEL <nplanel@gmail.com>

arch/M68K/M68Kdasm.c
@@ -2372,12 +2372,14 @@ static void d68020_extb_32(void)
 
 static void d68000_jmp(void)
 {
-	sprintf(g_dasm_str, "jmp     %s", get_ea_mode_str_32(g_cpu_ir));
+	build_bxx(M68K_INS_JMP, 0, peek_imm_32());
+	//sprintf(g_dasm_str, "jmp     %s", get_ea_mode_str_32(g_cpu_ir));
 }
 
 static void d68000_jsr(void)
 {
-	sprintf(g_dasm_str, "jsr     %s", get_ea_mode_str_32(g_cpu_ir));
+	build_bxx(M68K_INS_JSR, 0, peek_imm_32());
+	//sprintf(g_dasm_str, "jsr     %s", get_ea_mode_str_32(g_cpu_ir));
 }
 
 static void d68000_lea(void)

commit e41be15ca6d003de04e4e26debdbcd559f8cbc24

Fixed broken jmp/jst introduced in 0a27c1e

arch/M68K/M68Kdasm.c
@@ -2960,13 +2960,15 @@ static void d68020_extb_32(void)
 
 static void d68000_jmp(void)
 {
-	build_bxx(M68K_INS_JMP, 0, peek_imm_32());
+	cs_m68k* info = build_init_op(M68K_INS_JMP, 1, 0);
+	get_ea_mode_op(&info->operands[0], g_cpu_ir, 4);
 	//sprintf(g_dasm_str, "jmp     %s", get_ea_mode_str_32(g_cpu_ir));
 }
 
 static void d68000_jsr(void)
 {
-	build_bxx(M68K_INS_JSR, 0, peek_imm_32());
+	cs_m68k* info = build_init_op(M68K_INS_JSR, 1, 0);
+	get_ea_mode_op(&info->operands[0], g_cpu_ir, 4);
 	//sprintf(g_dasm_str, "jsr     %s", get_ea_mode_str_32(g_cpu_ir));
 }
 